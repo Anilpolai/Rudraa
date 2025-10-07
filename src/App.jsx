dcc7e5eebc01b1a9a4cff868f824e2cac6490d02
preneel <anil.polai03@gmail.com>
2025-10-07 15:51:14 +0530

    add the router

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
-import { useState } from 'react'
 import './App.css'
 import Home from './pages/home'
 import Page from './pages/page'
-import Product from './pages/prooduct'
+import Product from './pages/product'
 import Blog from './pages/blog'
+import Shop from './pages/shop'      // 👈 create this file if missing
+// import Cart from './pages/cart'      // 👈 optional, for cart page
 import Endlayout from './component/endlayout/endlayout'
-import {Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
-
   return (
-    <>
     <Routes>
-        <Route path="/" element={<Endlayout />}>
-          <Route index element={<Home />} />
-          <Route path='/page' element={<Page/>} />
-          <Route path='/prooduct' element={<Product/>} />
-          <Route path='/blog' element={<Blog />} />
-          
-          {/* <Route path="about" element={<About />} /> */}
-        </Route>
-      </Routes>
-    </>
+      <Route path="/" element={<Endlayout />}>
+        <Route index element={<Home />} />
+        <Route path="page" element={<Page />} />
+        <Route path="product" element={<Product />} />
+        <Route path="blog" element={<Blog />} />
+        <Route path="shop" element={<Shop />} />      
+        {/* {/* <Route path="cart" element={<Cart />} />       } */}
+      </Route>
+    </Routes>
   )
 }
 
