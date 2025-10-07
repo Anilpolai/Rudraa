a6fd1d371fa3e821f14c310c29fd92adcef0335e
preneel <anil.polai03@gmail.com>
2025-10-07 18:33:25 +0530

    add th tewlidd

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { CardBody, CardContainer, CardItem } from "../component/ui/3d-card";
 
-const product = () => {
+export function ThreeDCardDemo() {
   return (
-    <div>product</div>
-  )
+    <CardContainer className="inter-var" containerClassName="bg-gray-100 rounded-xl">
+      <CardBody className="bg-white rounded-xl shadow-xl">
+        <CardItem translateZ={50} rotateY={15} className="bg-red-400 h-24 w-24 rounded-lg" />
+        <CardItem translateZ={30} rotateX={10} className="bg-blue-400 h-20 w-20 rounded-lg absolute top-16 left-16" />
+      </CardBody>
+    </CardContainer>
+  );
 }
 
-export default product
+export default ThreeDCardDemo;
